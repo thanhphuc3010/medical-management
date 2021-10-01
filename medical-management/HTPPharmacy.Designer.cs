daa293c5d734cb574afa2e85cc45f4cfc82186eb
phuonganh75 <ngo5902@gmail.com>
2021-10-02 00:41:37 +0700

    Create MDI form

---
+++ medical-management/HTPPharmacy.Designer.cs
@@ -0,0 +1,369 @@
+﻿
+namespace medical_management
+{
+    partial class HTPPharmacy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsSystem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemReLogin = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemChangePassword = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsSales = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator10 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemPO = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator11 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemInvoice = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsReport = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator12 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemDT = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator13 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemPOReport = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator14 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemMedicalExpired = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator15 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemInventoryReport = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsCategory = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator16 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemThuoc = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator17 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemKH = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator18 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemNCC = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator19 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemNSX = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator20 = new System.Windows.Forms.ToolStripSeparator();
+            this.mItemNV = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsSystem,
+            this.tsCategory,
+            this.tsSales,
+            this.tsReport});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsSystem
+            // 
+            this.tsSystem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.mItemReLogin,
+            this.toolStripSeparator2,
+            this.mItemChangePassword,
+            this.toolStripSeparator3,
+            this.mItemHelp,
+            this.toolStripSeparator4,
+            this.mItemExit});
+            this.tsSystem.Name = "tsSystem";
+            this.tsSystem.Size = new System.Drawing.Size(69, 20);
+            this.tsSystem.Text = "Hệ thống";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mItemReLogin
+            // 
+            this.mItemReLogin.Name = "mItemReLogin";
+            this.mItemReLogin.Size = new System.Drawing.Size(180, 22);
+            this.mItemReLogin.Text = "Đăng nhập lại";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mItemChangePassword
+            // 
+            this.mItemChangePassword.Name = "mItemChangePassword";
+            this.mItemChangePassword.Size = new System.Drawing.Size(180, 22);
+            this.mItemChangePassword.Text = "Đổi mật khẩu";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mItemHelp
+            // 
+            this.mItemHelp.Name = "mItemHelp";
+            this.mItemHelp.Size = new System.Drawing.Size(180, 22);
+            this.mItemHelp.Text = "Trợ giúp";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mItemExit
+            // 
+            this.mItemExit.Name = "mItemExit";
+            this.mItemExit.Size = new System.Drawing.Size(180, 22);
+            this.mItemExit.Text = "Thoát";
+            // 
+            // tsSales
+            // 
+            this.tsSales.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator10,
+            this.mItemPO,
+            this.toolStripSeparator11,
+            this.mItemInvoice});
+            this.tsSales.Name = "tsSales";
+            this.tsSales.Size = new System.Drawing.Size(139, 20);
+            this.tsSales.Text = "Nhập hàng - Bán hàng";
+            // 
+            // toolStripSeparator10
+            // 
+            this.toolStripSeparator10.Name = "toolStripSeparator10";
+            this.toolStripSeparator10.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mItemPO
+            // 
+            this.mItemPO.Name = "mItemPO";
+            this.mItemPO.Size = new System.Drawing.Size(180, 22);
+            this.mItemPO.Text = "Hóa đơn nhập hàng";
+            // 
+            // toolStripSeparator11
+            // 
+            this.toolStripSeparator11.Name = "toolStripSeparator11";
+            this.toolStripSeparator11.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mItemInvoice
+            // 
+            this.mItemInvoice.Name = "mItemInvoice";
+            this.mItemInvoice.Size = new System.Drawing.Size(180, 22);
+            this.mItemInvoice.Text = "Hóa đơn bán hàng";
+            // 
+            // tsReport
+            // 
+            this.tsReport.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator12,
+            this.mItemDT,
+            this.toolStripSeparator13,
+            this.mItemPOReport,
+            this.toolStripSeparator14,
+            this.mItemMedicalExpired,
+            this.toolStripSeparator15,
+            this.mItemInventoryReport});
+            this.tsReport.Name = "tsReport";
+            this.tsReport.Size = new System.Drawing.Size(61, 20);
+            this.tsReport.Text = "Báo cáo";
+            // 
+            // toolStripSeparator12
+            // 
+            this.toolStripSeparator12.Name = "toolStripSeparator12";
+            this.toolStripSeparator12.Size = new System.Drawing.Size(190, 6);
+            // 
+            // mItemDT
+            // 
+            this.mItemDT.Name = "mItemDT";
+            this.mItemDT.Size = new System.Drawing.Size(193, 22);
+            this.mItemDT.Text = "Báo cáo doanh thu";
+            // 
+            // toolStripSeparator13
+            // 
+            this.toolStripSeparator13.Name = "toolStripSeparator13";
+            this.toolStripSeparator13.Size = new System.Drawing.Size(190, 6);
+            // 
+            // mItemPOReport
+            // 
+            this.mItemPOReport.Name = "mItemPOReport";
+            this.mItemPOReport.Size = new System.Drawing.Size(193, 22);
+            this.mItemPOReport.Text = "Báo cáo nhập hàng";
+            // 
+            // toolStripSeparator14
+            // 
+            this.toolStripSeparator14.Name = "toolStripSeparator14";
+            this.toolStripSeparator14.Size = new System.Drawing.Size(190, 6);
+            // 
+            // mItemMedicalExpired
+            // 
+            this.mItemMedicalExpired.Name = "mItemMedicalExpired";
+            this.mItemMedicalExpired.Size = new System.Drawing.Size(193, 22);
+            this.mItemMedicalExpired.Text = "Báo cáo thuốc hết hạn";
+            // 
+            // toolStripSeparator15
+            // 
+            this.toolStripSeparator15.Name = "toolStripSeparator15";
+            this.toolStripSeparator15.Size = new System.Drawing.Size(190, 6);
+            // 
+            // mItemInventoryReport
+            // 
+            this.mItemInventoryReport.Name = "mItemInventoryReport";
+            this.mItemInventoryReport.Size = new System.Drawing.Size(193, 22);
+            this.mItemInventoryReport.Text = "Báo cáo tồn kho";
+            // 
+            // tsCategory
+            // 
+            this.tsCategory.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator16,
+            this.mItemThuoc,
+            this.toolStripSeparator17,
+            this.mItemKH,
+            this.toolStripSeparator18,
+            this.mItemNCC,
+            this.toolStripSeparator19,
+            this.mItemNSX,
+            this.toolStripSeparator20,
+            this.mItemNV});
+            this.tsCategory.Name = "tsCategory";
+            this.tsCategory.Size = new System.Drawing.Size(74, 20);
+            this.tsCategory.Text = "Danh mục";
+            // 
+            // toolStripSeparator16
+            // 
+            this.toolStripSeparator16.Name = "toolStripSeparator16";
+            this.toolStripSeparator16.Size = new System.Drawing.Size(203, 6);
+            // 
+            // mItemThuoc
+            // 
+            this.mItemThuoc.Name = "mItemThuoc";
+            this.mItemThuoc.Size = new System.Drawing.Size(206, 22);
+            this.mItemThuoc.Text = "Danh mục Thuốc";
+            // 
+            // toolStripSeparator17
+            // 
+            this.toolStripSeparator17.Name = "toolStripSeparator17";
+            this.toolStripSeparator17.Size = new System.Drawing.Size(203, 6);
+            // 
+            // mItemKH
+            // 
+            this.mItemKH.Name = "mItemKH";
+            this.mItemKH.Size = new System.Drawing.Size(206, 22);
+            this.mItemKH.Text = "Danh mục Khách hàng";
+            // 
+            // toolStripSeparator18
+            // 
+            this.toolStripSeparator18.Name = "toolStripSeparator18";
+            this.toolStripSeparator18.Size = new System.Drawing.Size(203, 6);
+            // 
+            // mItemNCC
+            // 
+            this.mItemNCC.Name = "mItemNCC";
+            this.mItemNCC.Size = new System.Drawing.Size(206, 22);
+            this.mItemNCC.Text = "Danh mục Nhà cung cấp";
+            // 
+            // toolStripSeparator19
+            // 
+            this.toolStripSeparator19.Name = "toolStripSeparator19";
+            this.toolStripSeparator19.Size = new System.Drawing.Size(203, 6);
+            // 
+            // mItemNSX
+            // 
+            this.mItemNSX.Name = "mItemNSX";
+            this.mItemNSX.Size = new System.Drawing.Size(206, 22);
+            this.mItemNSX.Text = "Danh mục Nhà sản xuất";
+            // 
+            // toolStripSeparator20
+            // 
+            this.toolStripSeparator20.Name = "toolStripSeparator20";
+            this.toolStripSeparator20.Size = new System.Drawing.Size(203, 6);
+            // 
+            // mItemNV
+            // 
+            this.mItemNV.Name = "mItemNV";
+            this.mItemNV.Size = new System.Drawing.Size(206, 22);
+            this.mItemNV.Text = "Danh mục Nhân viên";
+            // 
+            // frmMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMDI";
+            this.Text = "MDI";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsSystem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mItemReLogin;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem mItemChangePassword;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem mItemHelp;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem mItemExit;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsSales;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator10;
+        private System.Windows.Forms.ToolStripMenuItem mItemPO;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator11;
+        private System.Windows.Forms.ToolStripMenuItem mItemInvoice;
+        private System.Windows.Forms.ToolStripMenuItem tsReport;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator12;
+        private System.Windows.Forms.ToolStripMenuItem mItemDT;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator13;
+        private System.Windows.Forms.ToolStripMenuItem mItemPOReport;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator14;
+        private System.Windows.Forms.ToolStripMenuItem mItemMedicalExpired;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator15;
+        private System.Windows.Forms.ToolStripMenuItem mItemInventoryReport;
+        private System.Windows.Forms.ToolStripMenuItem tsCategory;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator16;
+        private System.Windows.Forms.ToolStripMenuItem mItemThuoc;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator17;
+        private System.Windows.Forms.ToolStripMenuItem mItemKH;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator18;
+        private System.Windows.Forms.ToolStripMenuItem mItemNCC;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator19;
+        private System.Windows.Forms.ToolStripMenuItem mItemNSX;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator20;
+        private System.Windows.Forms.ToolStripMenuItem mItemNV;
+    }
+}
