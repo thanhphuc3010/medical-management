be0f70ea64d997130a9c3b2c03cc826b4ab59a27
phuonganh75 <ngo5902@gmail.com>
2021-10-27 17:00:42 +0700

    Create form SelectSupplier

---
+++ medical-management/Forms/Category/frmSelectSupplier.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace medical_management.Forms.Category
+{
+    partial class frmSelectSupplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgvSupplier = new System.Windows.Forms.DataGridView();
+            this.MaNCC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenNCC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Diachi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sdt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSupplier)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(52, 26);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(49, 13);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Tìm kiếm";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dgvSupplier
+            // 
+            this.dgvSupplier.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSupplier.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSupplier.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvSupplier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSupplier.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaNCC,
+            this.TenNCC,
+            this.Diachi,
+            this.Sdt});
+            this.dgvSupplier.Location = new System.Drawing.Point(12, 78);
+            this.dgvSupplier.Name = "dgvSupplier";
+            this.dgvSupplier.Size = new System.Drawing.Size(776, 360);
+            this.dgvSupplier.TabIndex = 2;
+            // 
+            // MaNCC
+            // 
+            this.MaNCC.DataPropertyName = "MaNCC";
+            this.MaNCC.HeaderText = "Mã Nhà cung cấp";
+            this.MaNCC.Name = "MaNCC";
+            // 
+            // TenNCC
+            // 
+            this.TenNCC.DataPropertyName = "TenNCC";
+            this.TenNCC.HeaderText = "Tên Nhà cung cấp";
+            this.TenNCC.Name = "TenNCC";
+            // 
+            // Diachi
+            // 
+            this.Diachi.DataPropertyName = "Diachi";
+            this.Diachi.HeaderText = "Địa chỉ";
+            this.Diachi.Name = "Diachi";
+            // 
+            // Sdt
+            // 
+            this.Sdt.DataPropertyName = "Sdt";
+            this.Sdt.HeaderText = "Số điện thoại";
+            this.Sdt.Name = "Sdt";
+            // 
+            // frmSelectSupplier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvSupplier);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSearch);
+            this.Name = "frmSelectSupplier";
+            this.Text = "frmSelectSupplier";
+            this.Load += new System.EventHandler(this.frmSelectSupplier_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSupplier)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dgvSupplier;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNCC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenNCC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Diachi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sdt;
+    }
+}
---
+++ medical-management/Forms/Category/frmSelectSupplier.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace medical_management.Forms.Category
+{
+    public partial class frmSelectSupplier : Form
+    {
+        public delegate void passSupplierId(string id, string name);
+        public passSupplierId publisher;
+
+        public frmSelectSupplier()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSelectSupplier_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void initializeUI()
+        {
+            foreach (DataGridViewRow row in dgvSupplier.Rows)
+            {
+                if (row.Index % 2 == 0)
+                {
+                    row.DefaultCellStyle.BackColor = Color.FromArgb(175, 238, 238);
+                }
+                else
+                {
+                    row.DefaultCellStyle.BackColor = Color.FromArgb(240, 255, 255);
+                }
+            }
+        }
+        private void loadData()
+        {
+            string query = "SELECT * FROM tbl_Customer";
+            DataTable data = Database.Instance.excuteQuery(query);
+            dgvSupplier.DataSource = data;
+            initializeUI();
+        }
+
+        private void dgvSupplier_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dgvSupplier.CurrentRow.Index;
+            string supplierId = dgvSupplier.Rows[index].Cells["MaNCC"].Value.ToString();
+            string supplierName = dgvSupplier.Rows[index].Cells["TenNCC"].Value.ToString();
+            publisher?.Invoke( supplierId, supplierName);
+            this.Close();
+        }
+    }
+
+}
