80fd48ff02da4dc8de95128202de904e0b713bdc
phucpt <69065925+thanhphuc3010@users.noreply.github.com>
2021-11-07 16:07:17 +0700

    Revert "Update invoice: auto fill staff is active into invoice when creating invoice "

--- medical-management/Forms/Authentication/frmLogin.Designer.cs
+++ medical-management/Forms/Authentication/frmLogin.Designer.cs
@@ -32,70 +32,42 @@ namespace medical_management
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(141, 70);
+            this.txtUserName.Location = new System.Drawing.Point(232, 150);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(340, 26);
+            this.txtUserName.Size = new System.Drawing.Size(340, 22);
             this.txtUserName.TabIndex = 0;
-            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(141, 109);
+            this.txtPassword.Location = new System.Drawing.Point(232, 193);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(340, 26);
-            this.txtPassword.TabIndex = 1;
-            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.Size = new System.Drawing.Size(340, 22);
+            this.txtPassword.TabIndex = 0;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(352, 159);
+            this.btnLogin.Location = new System.Drawing.Point(363, 238);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(129, 33);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Log in";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
-            // lblUsername
-            // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(26, 74);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(109, 18);
-            this.lblUsername.TabIndex = 2;
-            this.lblUsername.Text = "Tên đăng nhập:";
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(26, 113);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(74, 18);
-            this.lblPassword.TabIndex = 2;
-            this.lblPassword.Text = "Mật khẩu:";
-            // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 226);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.lblUsername);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUserName);
-            this.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmLogin";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "frmLogin";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,7 +78,5 @@ namespace medical_management
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.Label lblPassword;
     }
 }
--- medical-management/Forms/SalesInvoice/frmDSHD.cs
+++ medical-management/Forms/SalesInvoice/frmDSHD.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using medical_management.BUS;
 
 namespace medical_management
 {
@@ -19,40 +18,24 @@ namespace medical_management
         public frmDSHD()
         {
             InitializeComponent();
-            initializeUI();
         }
 
         private void btnAddInvoice_Click(object sender, EventArgs e)
         {
-            frmPhieubanhang f = new frmPhieubanhang(isCreate: true, frmDSHD: this);
-            f.reloadEventHandler += refreshEventHandler;
-            f.ShowDialog(this);
-        }
-
-        private void refreshEventHandler(object sender, frmPhieubanhang.ReloadEventArgs args)
-        {
-            loadWhenChildClosing(true);
+            frmPhieubanhang f = new frmPhieubanhang();
+            f.ShowDialog();
         }
 
         private void frmDSHD_Load(object sender, EventArgs e)
         {
             dtpFromDate.Value = getEarliestDate();
-            dtpToDate.Value = DateTime.Now;
+            dtpToDate.Value = getLatestDate();
             loadListInvoice();
             loadInvoiceSummary();
-            loadStatPayment(false);
-        }
-
-        private void loadWhenChildClosing(bool isReload)
-        {
-            dtpFromDate.Value = getEarliestDate();
-            dtpToDate.Value = DateTime.Now;
-            loadListInvoice();
-            loadInvoiceSummary();
-            loadStatPayment(false);
-            int lastRowIndex = dgvDSHD.Rows.Count - 1;
-            dgvDSHD.FirstDisplayedScrollingRowIndex = lastRowIndex;
-            dgvDSHD.Rows[lastRowIndex].Selected = true;
+            loadInvoiceCount();
+            loadTotal();
+            loadPayment();
+            loadReceivable();
         }
 
         private void initializeUI()
@@ -84,10 +67,9 @@ namespace medical_management
         {
             string query = "SELECT a.MaHD, a.NgayHD, a.Tongtien, (a.Tongtien - a.Dathanhtoan) AS Conthieu, a.Trangthaihoadon, b.TenKH, b.Sdt, c.TenNV " +
                            "FROM dbo.tbl_Invoice a, dbo.tbl_Customer b, dbo.tbl_Staff c " +
-                           "WHERE a.MaKH = b.MaKH AND a.MaNV = c.MaNV " +
-                           "AND NgayHD BETWEEN @FromDate AND @ToDate ";
+                           "WHERE a.MaKH = b.MaKH AND a.MaNV = c.MaNV";
 
-            DataTable data = Database.Instance.excuteQuery(query, new object[] { dtpFromDate.Value, dtpToDate.Value });
+            DataTable data = Database.Instance.excuteQuery(query);
 
             dgvDSHD.DataSource = data;
 
@@ -110,17 +92,37 @@ namespace medical_management
             dgvInvoiceSummary.DataSource = data;
         }
 
-        private void loadStatPayment(bool byFilter)
+        private void loadInvoiceCount()
         {
-            var paymentDictionary = byFilter ? InvoiceBUS.getPaymentSataticByFilter(fromDate, toDate, getListStatusFilter()) : InvoiceBUS.getPaymentSatatic();
-            int count = Convert.ToInt32(paymentDictionary["count"]);
-            decimal total = Convert.ToInt32(paymentDictionary["total"]);
-            decimal payment = Convert.ToInt32(paymentDictionary["payment"]);
+            string query = "SELECT COUNT(*) FROM dbo.tbl_Invoice";
+            lblInvoiceCount.Text = Database.Instance.ExecuteScalar(query).ToString();
+        }
 
-            lblInvoiceCount.Text = count.ToString();
-            lblTotal.Text = Helper.formatCurrencyVN(total);
-            lblTotalPayment.Text = Helper.formatCurrencyVN(payment);
-            lblReceivable.Text = Helper.formatCurrencyVN(total - payment);
+        private void loadTotal()
+        {
+            string query = "SELECT SUM(Tongtien) FROM dbo.tbl_Invoice";
+            decimal total = Convert.ToDecimal(Database.Instance.ExecuteScalar(query));
+
+            CultureInfo culture = new CultureInfo("vi-VN");
+            lblTotal.Text = total.ToString("c", culture);
+        }
+
+        private void loadPayment()
+        {
+            string query = "SELECT SUM(Dathanhtoan) FROM dbo.tbl_Invoice";
+            decimal total = Convert.ToDecimal(Database.Instance.ExecuteScalar(query));
+
+            CultureInfo culture = new CultureInfo("vi-VN");
+            lblTotalPayment.Text = total.ToString("c", culture);
+        }
+
+        private void loadReceivable()
+        {
+            string query = "SELECT SUM(Tongtien - Dathanhtoan) FROM dbo.tbl_Invoice";
+            decimal total = Convert.ToDecimal(Database.Instance.ExecuteScalar(query));
+
+            CultureInfo culture = new CultureInfo("vi-VN");
+            lblReceivable.Text = total.ToString("c", culture);
         }
 
         private DateTime getEarliestDate()
@@ -128,6 +130,12 @@ namespace medical_management
             string query = "SELECT TOP 1 NgayHD FROM dbo.tbl_Invoice ORDER BY NgayHD ASC";
             return Convert.ToDateTime(Database.Instance.ExecuteScalar(query));
         }
+        private DateTime getLatestDate()
+        {
+            string query = "SELECT TOP 1 NgayHD FROM dbo.tbl_Invoice ORDER BY NgayHD DESC";
+            return Convert.ToDateTime(Database.Instance.ExecuteScalar(query));
+        }
+
 
         private void dgvDSHD_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -136,60 +144,80 @@ namespace medical_management
                 string invoiceId = Convert.ToString(dgvDSHD.Rows[e.RowIndex].Cells[0].Value);
 
                 loadInvoiceDetailById(invoiceId);
-            }
-            else
+            } else
             {
                 initializeUI();
             }
-
+            
         }
 
         private void loadInvoiceDetailById(string invoiceId)
         {
-            DataTable data = InvoiceBUS.getInvoiceDetailById(invoiceId);
+            string query =
+                "SELECT a.Mathuoc, b.Tenthuoc, b.Donvi, a.Soluong, a.Dongia, (a.Soluong * a.Dongia) AS Thanhtien " +
+                "FROM dbo.tbl_InvoiceDetail a, dbo.tbl_Item b " +
+                "WHERE a.Mathuoc = b.Mathuoc " +
+                "AND MaHD = @MaHD ";
+            DataTable data = Database.Instance.excuteQuery(query, new object[] { invoiceId });
+
             dgvHoadonchitiet.DataSource = data;
         }
 
         private void btnFilter_Click(object sender, EventArgs e)
         {
-            fromDate = dtpFromDate.Value;
-            toDate = dtpToDate.Value;
+            DateTime fromDate = dtpFromDate.Value;
+            DateTime toDate = dtpToDate.Value;
 
             List<string> statusFilter = getListStatusFilter();
 
-            // Load Invoice Summary
+            string querySummary =
+                "SELECT DISTINCT Trangthaihoadon, COUNT(Trangthaihoadon) AS Soluong " +
+                "FROM dbo.tbl_Invoice " +
+                "WHERE NgayHD BETWEEN @FromDate AND @ToDate " +
+                "GROUP BY Trangthaihoadon";
 
-            DataTable dataSummary = InvoiceBUS.getInvoiceSummary(fromDate, toDate);
+            DataTable dataSummary = Database.Instance.excuteQuery(querySummary, new object[] { fromDate, toDate });
             dgvInvoiceSummary.DataSource = dataSummary;
 
-            DataTable data = new DataTable();
+            string query =
+                    "SELECT a.MaHD, a.NgayHD, a.Tongtien, (a.Tongtien - a.Dathanhtoan) AS Conthieu, a.Trangthaihoadon, b.TenKH, b.Sdt, c.TenNV " +
+                    "FROM dbo.tbl_Invoice a, dbo.tbl_Customer b, dbo.tbl_Staff c " +
+                    "WHERE a.MaKH = b.MaKH AND a.MaNV = c.MaNV " +
+                    "AND NgayHD BETWEEN @FromDate AND @ToDate " +
+                    "AND Trangthaihoadon IN (";
 
             if (statusFilter.Count == 0)
             {
-                data = InvoiceBUS.getAllInvoiceToNow(fromDate, toDate);
-                loadStatPayment(true);
-            } else
-            {
-                data = InvoiceBUS.getInvoiceWithFilter(fromDate, toDate, statusFilter);
-                loadStatPayment(true);
-            }
-
-            dgvDSHD.DataSource = data;
-
-            initializeUI();
-
-            handleLoadInvoiceDetail(data);
-        }
-
-        private void handleLoadInvoiceDetail(DataTable data)
-        {
-            if (dgvDSHD.Rows.Count > 0 && data.Rows.Count != 0)
-            {
-                loadInvoiceDetailById(dgvDSHD.Rows[0].Cells["MaHD"].Value.ToString());
+                query = "SELECT a.MaHD, a.NgayHD, a.Tongtien, (a.Tongtien - a.Dathanhtoan) AS Conthieu, a.Trangthaihoadon, b.TenKH, b.Sdt, c.TenNV " +
+                    "FROM dbo.tbl_Invoice a, dbo.tbl_Customer b, dbo.tbl_Staff c " +
+                    "WHERE a.MaKH = b.MaKH AND a.MaNV = c.MaNV " +
+                    "AND NgayHD BETWEEN @FromDate AND @ToDate ";
             }
             else
             {
-                dgvHoadonchitiet.DataSource = null;
+                var lastItem = statusFilter.Last();
+
+                foreach (string status in statusFilter)
+                {
+                    if (!status.Equals(lastItem))
+                    {
+                        query += "N'" + status + "', ";
+                    }
+                    else
+                    {
+                        query += "N'" + status + "')";
+                    }
+
+                }
+            }
+
+            DataTable data = Database.Instance.excuteQuery(query, new object[] { fromDate, toDate });
+
+            dgvDSHD.DataSource = data;
+
+            if (!dgvDSHD.Rows[0].IsNewRow)
+            {
+                loadInvoiceDetailById(dgvDSHD.Rows[0].Cells["MaHD"].Value.ToString());
             }
         }
 
@@ -218,24 +246,46 @@ namespace medical_management
 
                 List<string> statusFilter = getListStatusFilter();
 
-                fromDate = dtpFromDate.Value;
-                toDate = dtpToDate.Value;
+                DateTime fromDate = dtpFromDate.Value;
+                DateTime toDate = dtpToDate.Value;
+
+                string query = 
+                        "SELECT a.MaHD, a.NgayHD, a.Tongtien, (a.Tongtien - a.Dathanhtoan) AS Conthieu, a.Trangthaihoadon, b.TenKH, b.Sdt, c.TenNV " +
+                        "FROM dbo.tbl_Invoice a, dbo.tbl_Customer b, dbo.tbl_Staff c " +
+                        "WHERE a.MaKH = b.MaKH AND a.MaNV = c.MaNV " +
+                        "AND NgayHD BETWEEN @FromDate AND @ToDate " +
+                        "AND Trangthaihoadon IN (";
 
                 if (statusFilter.Count == 0)
                 {
                     loadListInvoice();
-                    loadStatPayment(true);
                     return;
                 }
+                var lastItem = statusFilter.Last();
 
-                DataTable data = InvoiceBUS.getInvoiceWithFilter(fromDate, toDate, statusFilter);
+                foreach (string status in statusFilter)
+                {
+                    if(!status.Equals(lastItem))
+                    {
+                        query += "N'" + status + "', ";
+                    } else
+                    {
+                        query += "N'" + status + "')";
+                    }
+                    
+                }
+
+                DataTable data = Database.Instance.excuteQuery(query, new object[] { fromDate, toDate });
 
                 dgvDSHD.DataSource = data;
 
-                initializeUI();
-                loadStatPayment(true);
-
-                handleLoadInvoiceDetail(data);
+                if (dgvDSHD.Rows.Count > 0 && data.Rows.Count != 0)
+                {
+                    loadInvoiceDetailById(dgvDSHD.Rows[0].Cells["MaHD"].Value.ToString());
+                } else
+                {
+                    dgvHoadonchitiet.DataSource = null;
+                }
             }
         }
 
@@ -249,38 +299,6 @@ namespace medical_management
 
         private void dgvDSHD_Sorted(object sender, EventArgs e)
         {
-            initializeUI();
-        }
-
-        private void dgvDSHD_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            try
-            {
-                dgvDSHD.CurrentCell = dgvDSHD.Rows[e.RowIndex].Cells[e.ColumnIndex];
-                dgvDSHD.Rows[e.RowIndex].Selected = true;
-                dgvDSHD.Focus();
-                if (dgvDSHD.Rows[e.RowIndex].IsNewRow)
-                {
-                    return;
-                }
-                else
-                {
-                    string invoiceId = Convert.ToString(dgvDSHD.Rows[e.RowIndex].Cells[0].Value);
-                    frmPhieubanhang f = new frmPhieubanhang(isCreate: false, this, invoiceId);
-                    f.reloadEventHandler += refreshEventHandler;
-                    f.ShowDialog(this);
-                }
-
-            }
-            catch (Exception err)
-            {
-                Helper.showErrorMessage(err.Message);
-            }
-        }
-
-        private void frmDSHD_Shown(object sender, EventArgs e)
-        {
-            // Handle UI when forms shown at the first times
             initializeUI();
         }
     }
